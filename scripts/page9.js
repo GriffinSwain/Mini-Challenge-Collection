1c735d047847c1752f93d9e0fb65b955048a2788
Griffin <116022056+GriffinSwain@users.noreply.github.com>
2023-01-13 16:55:43 -0800

    Finished Product

--- scripts/page9.js
+++ scripts/page9.js
@@ -1,9 +1,25 @@
-let textInput = document.getElementById("textInput");
+// let textInput = document.getElementById("textInput");
+// let textInput = "bad";
 let textReturn = document.getElementById("textReturn");
+let badSubBtn = document.getElementById("badSubBtn");
+let decentSubBtn = document.getElementById("decentSubBtn");
+let goodSubBtn = document.getElementById("goodSubBtn");
 let textSubBtn = document.getElementById("textSubBtn");
 
 let savedInput = "";
 let RestaurantPickerUrl = "";
+
+badSubBtn.addEventListener("click", function () {
+    textInput = "bad";
+})
+
+goodSubBtn.addEventListener("click", function () {
+    textInput = "good";
+})
+
+decentSubBtn.addEventListener("click", function () {
+    textInput = "decent";
+})
 
 textSubBtn.addEventListener("click", function () {
     RestaurantPickerApi(savedInput)
@@ -22,13 +38,18 @@ textSubBtn.addEventListener("click", function () {
     
     function RestaurantPickerApi(savedInput){
 
-        if (textInput == "good" || "bad" || "decent"){
-
-            savedInput = textInput.value
+        if (textInput == "bad"){
+            savedInput = "bad";
+        }
+        if (textInput == "good"){
+            savedInput = "good";
+        }
+        if (textInput == "decent"){
+            savedInput = "decent";
+        }
+        if (textInput == ""){
+            textReturn = "Please click a button"
+        }
             RestaurantPickerUrl = "https://griffinminichapi.azurewebsites.net/minichallenge/RestaurantPicker/" + savedInput;
             urlCall(RestaurantPickerUrl)
         }
-        else{
-            textReturn.textContent = "Enter a valid response";
-        }
-    }
